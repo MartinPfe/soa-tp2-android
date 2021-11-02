f50d89bae2b28a64de1f3e6cdb51e3f39dbc521f
TomDiem <tomas.dellamaddalena@hotmail.com>
2021-11-01 21:48:22 -0300

    Refactor Login

--- app/src/main/java/com/example/tp2_grupo4/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/tp2_grupo4/ui/login/LoginActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
@@ -26,10 +28,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tp2_grupo4.HttpClient.HttpCliente_POST;
+import com.example.tp2_grupo4.MainActivity;
 import com.example.tp2_grupo4.R;
-import com.example.tp2_grupo4.data.model.User;
-import com.example.tp2_grupo4.helpers.ReceptorOperacionLogin;
-import com.example.tp2_grupo4.helpers.ReceptorOperacionRegistro;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -202,8 +203,7 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
-    private void configurarBroadcastReceiver()
-    {
+    private void configurarBroadcastReceiver() {
         filtroRegistro = new IntentFilter("com.example.intentservice.intent.action.RESPUESTA_REGISTRO");
         filtroRegistro.addCategory(Intent.CATEGORY_DEFAULT);
         registerReceiver(receiverRegistro, filtroRegistro);
@@ -211,6 +211,81 @@ public class LoginActivity extends AppCompatActivity {
         filtroLogin = new IntentFilter("com.example.intentservice.intent.action.RESPUESTA_LOGIN");
         filtroLogin.addCategory(Intent.CATEGORY_DEFAULT);
         registerReceiver(receiverLogin, filtroLogin);
+    }
+
+    public class ReceptorOperacionLogin extends BroadcastReceiver
+    {
+        public void onReceive(Context context, Intent intent) {
+            try {
+                String datosJsonString = intent.getStringExtra("datosJson");
+
+                if(datosJsonString != "NO_OK")
+                {
+                    JSONObject datosJson = new JSONObject(datosJsonString);
+                    Boolean success = datosJson.getBoolean("success");
+
+                    if(success)
+                    {
+                        String token = datosJson.getString("token");
+                        /// //TODO: Guardar el token para futuro usao
+                        Toast.makeText(context, "Sesión iniciada correctamente", Toast.LENGTH_SHORT).show();
+
+                        Intent mainActivityIntent = new Intent(context, MainActivity.class);
+                        context.startActivity(mainActivityIntent);
+                    }
+                    else
+                    {
+                        String mensaje = datosJson.getString("msg");
+                        Toast.makeText(context, "Ocurrió un error autenticando al usuario: " + mensaje, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(context, "Ocurrió un error autenticando al usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public class ReceptorOperacionRegistro extends BroadcastReceiver
+    {
+        public void onReceive(Context context, Intent intent) {
+            try {
+                String datosJsonString = intent.getStringExtra("datosJson");
+
+                if(datosJsonString != "NO_OK")
+                {
+                    JSONObject datosJson = new JSONObject(datosJsonString);
+                    Boolean success = datosJson.getBoolean("success");
+
+                    if(success)
+                    {
+                        String token = datosJson.getString("token");
+                        /// //TODO: Guardar el token para futuro usao
+                        Toast.makeText(context, "Sesión iniciada correctamente", Toast.LENGTH_SHORT).show();
+
+                        Intent mainActivityIntent = new Intent(context, MainActivity.class);
+                        context.startActivity(mainActivityIntent);
+                    }
+                    else
+                    {
+                        String mensaje = datosJson.getString("msg");
+                        Toast.makeText(context, "Ocurrió un error registrando al usuario: " + mensaje, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(context, "Ocurrió un error registrando al usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
